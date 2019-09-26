699bdf078150e62d2961c62f0122fe648bdc9c8c
potados99 <potados99@gmail.com>
2019-09-26 13:44:38 +0900

    Rename functions

--- sort/statistics.py
+++ sort/statistics.py
@@ -4,11 +4,11 @@ from util import *
 from sort import *
 from QuickSort import *
 
-# Print x in iteration and its results as y.
+# Print x in x_range and their results as y.
 # Useful when drawing a 2D plot in MATLAB.
-def dump_result(function, iteration, x_name="", y_name=""):
-    x = iteration
-    y = map(function, iteration)
+def dump_function_xy(function, x_range, x_name="", y_name=""):
+    x = x_range
+    y = map(function, x_range)
 
     if x_name != "":
         print(x_name + " = " + str(x))
@@ -18,27 +18,21 @@ def dump_result(function, iteration, x_name="", y_name=""):
 
 # This function creates output that can be interpreted by MATLAB.
 # Just copy and past the output, MATLAB will show them on plot.
-def sorting_test(name, sort_function, data_range):
-    dump_result(
-        lambda x: elapsed_time(
-            lambda: sort_function(random_list(x))
-        ),
+def dump_sort_function(name, sort_function, data_range):
+    dump_function_xy(
+        lambda x: elapsed_time(lambda: sort_function(random_list(x))),
         data_range,
         "x",
         "y1"
     )
-    dump_result(
-        lambda x: elapsed_time(
-            lambda: sort_function(ordered_list(x))
-        ),
+    dump_function_xy(
+        lambda x: elapsed_time(lambda: sort_function(ordered_list(x))),
         data_range,
         "",
         "y2"
     )
-    dump_result(
-        lambda x: elapsed_time(
-            lambda: sort_function(reverse_ordered_list(x))
-        ),
+    dump_function_xy(
+        lambda x: elapsed_time(lambda: sort_function(reverse_ordered_list(x))),
         data_range,
         "",
         "y3"
@@ -51,23 +45,27 @@ def sorting_test(name, sort_function, data_range):
     print("legend({'random data', 'ordered data', 'reverse ordered data'}, 'Location', 'northwest')")
     print("")
 
-x_range = range(1, 1000, 5)
-qsorter = QuickSort()
+# Test all
+def run():
+    x_range = range(1, 1000, 5)
+    qsorter = QuickSort()
 
-sorting_test("Selection Sort", selection_sort, x_range)
+    dump_sort_function("Selection Sort", selection_sort, x_range)
 
-sorting_test("Insertion Sort", insertion_sort, x_range)
+    dump_sort_function("Insertion Sort", insertion_sort, x_range)
 
-sorting_test("Bubble Sort", bubble_sort, x_range)
+    dump_sort_function("Bubble Sort", bubble_sort, x_range)
 
-qsorter.set_optimization(mid_pivot=False, min_partition=0)
-sorting_test("Quick Sort Non-Optimized", qsorter.sort, x_range)
+    qsorter.set_optimization(mid_pivot=False, min_partition=0)
+    dump_sort_function("Quick Sort Non-Optimized", qsorter.sort, x_range)
 
-qsorter.set_optimization(mid_pivot=True, min_partition=0)
-sorting_test("Quick Sort Using Mid Pivot", qsorter.sort, x_range)
+    qsorter.set_optimization(mid_pivot=True, min_partition=0)
+    dump_sort_function("Quick Sort Using Mid Pivot", qsorter.sort, x_range)
 
-qsorter.set_optimization(mid_pivot=False, min_partition=38)
-sorting_test("Quick Sort Using Insertion Sort", qsorter.sort, x_range)
+    qsorter.set_optimization(mid_pivot=False, min_partition=38)
+    dump_sort_function("Quick Sort Using Insertion Sort", qsorter.sort, x_range)
 
-qsorter.set_optimization(mid_pivot=True, min_partition=38)
-sorting_test("Quick Sort Optimized", qsorter.sort, x_range)
+    qsorter.set_optimization(mid_pivot=True, min_partition=38)
+    dump_sort_function("Quick Sort Optimized", qsorter.sort, x_range)
+
+run()
