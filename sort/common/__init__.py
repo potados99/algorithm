31a2ac9a4a38177beb52c4a2fb48e28ea026410f
potados99 <potados99@gmail.com>
2019-10-10 14:43:04 +0900

    Add comments in 'common'.

---
+++ sort/common/__init__.py
@@ -0,0 +1,6 @@
+"""Common things for sorting algorithms.
+
+This submodule includes utils and dump functions for testing the sort module.
+They might not be used in the sorting functions, but they stay here
+for its own testability.
+"""
