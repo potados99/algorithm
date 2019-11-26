e428a824449575dfc199fcc1843e9bf4a27a1053
potados99 <potados99@gmail.com>
2019-11-27 02:22:00 +0900

    Regex works!

--- search/regex.py
+++ search/regex.py
@@ -64,7 +64,7 @@ def concat(first_nfa, second_nfa):
     add_epsilon_transition(first_nfa.end, second_nfa.start)
     first_nfa.end.is_end = False
 
-    return create_NFA(first_nfa.end, second_nfa.start)
+    return create_NFA(first_nfa.start, second_nfa.end)
 
 
 def union(first_nfa, second_nfa):
@@ -74,8 +74,8 @@ def union(first_nfa, second_nfa):
 
     end_state = create_state(True)
     add_epsilon_transition(first_nfa.end, end_state)
-    add_epsilon_transition(second_nfa.end, end_state)
     first_nfa.end.is_end = False
+    add_epsilon_transition(second_nfa.end, end_state)
     second_nfa.end.is_end = False
 
     return create_NFA(start_state, end_state)
@@ -102,48 +102,80 @@ def to_NFA(postfix_exp):
     stack = []
 
     for c in postfix_exp:
+        print(c)
+
         if c == '*':
             stack.append(closure(stack.pop()))
-            print("*!")
-            [print(str(i)) for i in stack]
         elif c == '|':
             right = stack.pop()
             left = stack.pop()
             stack.append(union(left, right))
-            print("|!")
-            [print(str(i)) for i in stack]
         elif c == '.':
             right = stack.pop()
             left = stack.pop()
             stack.append(concat(left, right))
-            print(".!")
-            [print(str(i)) for i in stack]
         else:
             stack.append(from_symbol(c))
-            print(str(c) + " !")
-            [print(str(i)) for i in stack]
-
-        [print(str(i)) for i in stack]
 
     return stack.pop()
+
+
+def add_next_state(state, next_states, visited):
+    if len(state.epsilon_transitions) > 0:
+        for st in state.epsilon_transitions:
+            if not state in next_states:
+                visited.append(state)
+                add_next_state(st, next_states, visited)
+    else:
+        next_states.append(state)
+
+
+def search(nfa, word):
+    current_states = []
+    add_next_state(nfa.start, current_states, [])
+
+    for c in word:
+        next_states = []
+
+        for state in current_states:
+            if c in state.transition:
+                add_next_state(state.transition[c], next_states, [])
+
+        current_states = next_states
+
+    return next((x for x in current_states if x.is_end), None) is not None
+
+
+def create_matcher(postfix_exp):
+    nfa = to_NFA(postfix_exp);
+
+    return lambda word: search(nfa, word);
 
 
 def dump_NFA(nfa):
     dump_state(nfa.start)
 
 
-def dump_state(state):
+def dump_state(state, iterated=[]):
+    if state in iterated: return
+
     print("State: " + str(state))
     print("    is_end: " + str(state.is_end))
     print("    transition: " + str(state.transition))
     print("    epsilon_transitions: " + str(state.epsilon_transitions))
     print("")
 
+    iterated.append(state)
+
     if len(state.transition) == 0 and len(state.epsilon_transitions) == 0:
         return
+    if state.is_end:
+        return
     else:
-        for symbol in state.transition: dump_state(state.transition[symbol])
-        for state in state.epsilon_transitions: dump_state(state)
+        for symbol in state.transition:
+            dump_state(state.transition[symbol], iterated)
+        for state in state.epsilon_transitions:
+            dump_state(state, iterated)
 
 
 def regex(pattern, text, verbose=False):
@@ -184,8 +216,13 @@ if __name__ == "__main__":
     dump_NFA(nfa_a_and_b)
     """
 
-    nfa = to_NFA("ab∣*c.")
-    dump_NFA(nfa)
+    matcher = create_matcher("ab|*c.")
+    print(matcher("abc"))
+    print(matcher("ac"))
+    print(matcher("bc"))
+    print(matcher("abbc"))
+    print(matcher("adc"))
+
 
     #from common.invoker import from_input
     #from_input(regex)
