4153ce19e45aa200dba1a7d32c207977c19c069c
potados99 <potados99@gmail.com>
2019-12-13 20:43:22 +0900

    Works.

--- search/patricia.py
+++ search/patricia.py
@@ -39,24 +39,27 @@ class Node:
         self.skipped_bit: int = skipped_bit
 
     def __str__(self):
-        return "Key: " + str(self.key) + ", Skipped bit: " + str(self.skipped_bit) + "."
+        return "Key: " + str(self.key) + ", Skipped bit: " + str(self.skipped_bit) + ", Left: " + str(self.left.key) + ", Right: " + str(self.right.key) + "."
 
 
 class Dict:
     """
     >>> Dict.search_test(26)
     'Success'
+    >>> Dict.insert_test()
 
     """
     def __init__(self):
-        self.key_min: Bitskey = Bitskey(0)
+        self.key_min: Bitskey = Bitskey(-1)
         self.head: Node = Node(self.key_min)
 
-        self.head.skipped_bit = 14
-        self.head.left = self.head
-        self.head.right = self.head
+    def is_empty(self):
+        return self.head.key == self.key_min
 
     def select_child(self, key_to_find: Bitskey, parent: Bitskey, verbose=False):
+        """
+        Select a child node given key and the offset of the parent node.
+        """
         if key_to_find.cmp(offset=parent.skipped_bit, operand=1):
             if verbose: print("Go right.")
             selected = parent.right
@@ -71,38 +74,128 @@ class Dict:
 
         return selected
 
-    def search(self, key_to_find: Bitskey, verbose=False):
+    def find_node_top_down(self, key_to_find: Bitskey, until: int=-1, verbose=False):
+        """
+        Find node that contains 'key_to_find'.
+        If failed, it returns a closest node.
+        We can limit the skipped bit using 'until'.
+        """
         parent: Node = self.head
-        child: Node = self.head.left
+        child: Node = self.select_child(key_to_find=key_to_find, parent=parent, verbose=verbose)
 
-        child = self.select_child(key_to_find, parent)
-
-        while parent.skipped_bit > child.skipped_bit:
+        while parent.skipped_bit > child.skipped_bit and child.skipped_bit > until:
             parent = child
 
             if verbose:
                 print("Step down to next level.")
                 print("Skipped bit is at " + str(child.skipped_bit) + ".")
 
-            child = self.select_child(key_to_find=key_to_find, parent=child)
+            child = self.select_child(key_to_find=key_to_find, parent=child, verbose=verbose)
 
-        if key_to_find == child.key:
+        return (parent, child)
+
+    def find_closest_node(self, key_to_find: Bitskey, verbose=False):
+        """
+        Find the closest node, the last node visited when search is failed.
+        """
+        node: Node = self.find_node_top_down(key_to_find=key_to_find, verbose=verbose)[1]
+        return node if node.key != key_to_find else None
+
+    def find_proper_parent_and_child(self, key_to_find: Bitskey, skipped_bit: int, verbose=False):
+        """
+        Find a proper node that should be a child node for a new node of key 'key_to_find'.
+        """
+        return self.find_node_top_down(key_to_find=key_to_find, until=skipped_bit, verbose=verbose)
+
+    def search(self, key_to_find: Bitskey, for_insert=False, verbose=False):
+        """
+        Perform a search using key.
+
+        Params:
+            key_to_find(Bitskey): A key to find in a tree(trie).
+
+        Returns:
+            Bitskey: Matching key on success, minimum key on failure.
+        """
+        node: Node = self.find_node_top_down(key_to_find=key_to_find, verbose=verbose)[1]
+
+        if key_to_find == node.key:
             if verbose: print("The key(" + str(key_to_find) + ") is found.")
-            return child.key
+            return node.key
         else:
             if verbose: print("The key(" + str(key_to_find) + ") is not found.")
             return self.key_min
 
-    def insert(self, key: Bitskey, verbose=False):
-        print("Insert called!")
-        # Check if it exists
-        result: Bitskey = self.search(key)
-        print(result)
-        if result.get() == key.get():
-            # Already exists!
-            print("Already here!")
+    def insert(self, key_to_insert: Bitskey, max_width=14, verbose=False):
+        if self.is_empty():
+            # First time insert.
+            skipped_bit = max_width
+            while key_to_insert.cmp(skipped_bit, 0): skipped_bit -= 1
+
+            self.head = Node(key=key_to_insert, skipped_bit=skipped_bit)
+
             return
-        pass
+
+        # Check if it exists.
+        closest_node: Node = self.find_closest_node(key_to_find=key_to_insert, verbose=verbose)
+        if closest_node is None: return
+
+        # Find proper skipped_bit for the new key.
+        skipped_bit = max_width
+        while closest_node.key.cmp(skipped_bit, 1) == key_to_insert.cmp(skipped_bit, 1):
+            skipped_bit -= 1
+
+        # Find proper parent and child. The new node will be between them.
+        parent, child = self.find_proper_parent_and_child(key_to_find=key_to_insert, skipped_bit=skipped_bit, verbose=verbose)
+
+        # Create new node.
+        new_node: Node = Node(key=key_to_insert, skipped_bit=skipped_bit)
+
+        # Set direction of the child node.
+        if key_to_insert.cmp(new_node.skipped_bit, 1):
+            new_node.left = child
+        else:
+            new_node.right = child
+
+
+        print("===============")
+        print(parent)
+        print(child)
+        print(new_node)
+        print("===============")
+
+        # Set direction of the new node.
+        parent_and_child_are_head = (parent == child and child == self.head)
+        head_will_be_chagned = (new_node.skipped_bit > self.head.skipped_bit)
+
+        if parent_and_child_are_head and head_will_be_chagned:
+            # Do nothing. It happens on the second insertion.
+            pass
+        else:
+            if key_to_insert.cmp(parent.skipped_bit, 1):
+                parent.right = new_node
+            else:
+                parent.left = new_node
+
+        # Update header if new node becomes root.
+        if new_node.skipped_bit > self.head.skipped_bit:
+            print("YA!")
+            self.head = new_node
+
+    def dump(self, root=None, visited=[]):
+        if root in visited:
+            return
+
+        if root is None:
+            root = self.head
+
+        print(root)
+        visited.append(root)
+
+        if root.left != root:
+            self.dump(root.left)
+        if root.right != root:
+            self.dump(root.right)
 
     @staticmethod
     def search_test(key_to_find=3):
@@ -119,9 +212,7 @@ class Dict:
         dict.head = node19
         dict.key_min = Bitskey(1)
 
-        result: Bitskey = dict.search(Bitskey(key_to_find), verbose=True)
-
-        #dict.insert(Bitskey(26))
+        result: Bitskey = dict.search(Bitskey(key_to_find), verbose=False)
 
         if result.get() == key_to_find:
             return "Success"
@@ -130,7 +221,19 @@ class Dict:
 
     @staticmethod
     def insert_test():
-        return "NotImplemented"
+        dict: Dict = Dict()
+
+        dict.insert(Bitskey(1))
+        dict.insert(Bitskey(19))
+        dict.insert(Bitskey(5))
+        dict.insert(Bitskey(18))
+        #dict.insert(Bitskey(3))
+        #dict.insert(Bitskey(26))
+        #dict.insert(Bitskey(9))
+
+        dict.dump()
+
+
 
 
 if __name__ == "__main__":
